fa5b6e004890a5e03744eb470cbb9e52c1f97ed0
Benjamin West <benjamin.west496@my.tccd.edu>
2021-09-20 19:26:17 -0500

    Testing

--- classwork/HelloWorld/HelloWorld/Program.cs
+++ classwork/HelloWorld/HelloWorld/Program.cs
@@ -1,13 +1,208 @@
 ﻿using System;
+using System.Text;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main ( string[] args )
         {
-            xyz
+            //single line
+            int xyz;
             Console.WriteLine("Hello World!");
+
+            sbyte code = 10;
+            short port = 1234;
+            int hours = 45;
+            long debtPayBackInYears = 10000;
+
+            byte errorCode = 0xAF;
+            ushort errorPort = 0x145;
+            uint unpaidHours = 0;
+
+            double hoursWorked = 80.9;
+            decimal payRate = 12.50M;
+
+            char letter = 'A';
+            string name = "Ben";
+
+            //Not primative 
+            DateTime today = DateTime.Now;
+            TimeSpan interval = TimeSpan.FromMinutes(10);
+
+            static void DemoLogicalOperators()
+            {
+                //Logical AND - true if both operands are true
+                //logical OR - true if either operand is true
+
+                //NOT ! - inverts expression
+
+            }
+
+            static void DemoRelationalOperators ()
+            {
+                int x = 10, y = 20;
+
+                bool isLessThan = x < y;
+                bool isLessThanOrEqualTo = x <= y;
+
+                bool isGreaterThan = x > y;
+                bool isGreaterThanOrEqualTo = x >= y;
+
+                bool isEqual = x == y;
+                bool isNotEqual = x != y;
+            }
+
+           static void DemoCombinationOperators ()
+            {
+                int x = 10;
+
+                x += 10; // x = x+10
+                x -= 10; // x = x-10;
+                x *= 3; // x = x*3;
+                x /= 5; // x = x/5
+                x %= 2; // x = x%2
+            }
+
+            static void DemoPrefixPostfixOperators ()
+            {
+                int x = 10, y;
+
+                //Prefix evaluates then assigns
+                y = ++x;
+                y = --x;
+
+                //Postfix assigns then evaluates
+                y = x++;
+                y = x--;
+
+
+            }
+
+            static void DemoAssignmentOperator ()
+            {
+                int x;
+                int y;
+                int z;
+
+                x = 10;
+                y = 10;
+                z = 10;
+
+                // left associative operators (E1 to E2) => E1, E2 op
+                //right associative operators
+
+                y = x = y = z = 20; //x = (y = (z = 20))
+            }
+
+            static void ConditionalOperator()
+            {
+                int grade = 70;
+
+                string passStatus;
+
+                if (grade < 60)
+                    passStatus = "Not Passing";
+                 else
+                    passStatus = "Passing";
+
+                //Terninary - operands
+                // E(bool) ?E(True) : E(false)
+
+                string passStatus2 = (grade < 60) ? "Not Passing" : "Passing";
+            }
+
+            static void DemoStrings()
+            {
+                string firstName = "Ben";
+                // Case sensative
+                bool isBen = firstName == "Ben";
+
+                //String literals \? only work in string literals
+                // \n newline
+                // \t horizontal tab
+                // \\ literal \
+                // \" double quote
+                // \' character literal, single quote
+                string literall = "Hello World";
+                literall = "Hello\nWorld";
+
+                //String length - >= how many characters
+                int length = literall.Length;
+
+                //Empty string
+                string emptyString = ""; //.length = 0;
+                string emptyString2 = String.Empty; // Length 0
+                bool areEmptyStringsEqual = String.Empty == ""; // True 
+
+                //null != empty string
+                // default value for a string is null
+                string nullString = null;
+                bool areEqualNull = "" == null; // false
+
+                // string is the universal type in C#
+                // all expressions are convertable to string using .Tostring
+                string asString = length.ToString(); // length as a string
+                asString = 10.ToString(); // 10
+                asString = areEqualNull.ToString(); // false
+
+
+                //comparison
+                int year = 2021;
+                string value1 = "Hello", value2 = "hello";
+                bool areEqual;
+                areEqual = value1 == value2;
+                areEqual = String.Compare(value1, value2) == 0; //prefered
+
+                bool compareCaseInsensitive = value1.ToUpper() == value2.ToUpper();
+
+                //Concatenation
+                string firstName1 = "Bob";
+                string lastName1 = "Smith";
+                string name = firstName1 + " " + lastName1; // Bob Smith
+                String.Concat(firstName1, " ", lastName1, " ", year); // same as upper
+
+                var builder = new StringBuilder();
+                builder.Append(firstName);
+                builder.Append(" ");
+                builder.Append(lastName1);
+                builder.Append(" ");
+                builder.Append(year);
+                name = builder.ToString();
+
+                name = String.Join(" ", firstName1, lastName1, year);
+
+                //Misc
+                bool startsWithB = name.StartsWith("B");
+                startsWithB = name.StartsWith("B", StringComparison.CurrentCultureIgnoreCase);
+
+                //empty strings
+
+                bool isEmpty;
+                isEmpty = name == ""; // not always going to work properly
+                isEmpty = name.Length == 0; //will crash if null
+
+                //handle null
+                isEmpty = (name != null) ? name == "" : true;
+                isEmpty = name == null || name == "";
+                isEmpty = (name != null) ? name.Length == 0 : true;
+                isEmpty = String.IsNullOrEmpty(name); // prefered
+
+                //C style formating
+                name = String.Format("Hello {0} {1}, the year is {2}.", firstName1, lastName1, year);
+
+                decimal price = 8.75M;
+                string priceString = price.ToString(); // 8.75000
+                priceString = price.ToString("C"); // Money
+                priceString = price.ToString("N6"); //8.7500
+                priceString = String.Format("{0:C}", price); //
+
+                //String interpolation - way to go
+                name = $"Hello {firstName1} {lastName1}, the year is {year}.";
+
+
+            }
+
         }
     }
 }
---
+++ classwork/HelloWorld/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+
+namespace MovieLibrary.ConsoleHost
+{
+    class Program
+    {
+        static void Main ( string[] args )
+        {
+            bool done = false;
+
+            do
+            {
+                char choice = GetInput();
+                //if (choice == 'Q')
+                //    done = HandleQuit();
+                //else if (choice == 'A')
+                //    AddMovie();
+                //else if (choice == 'V')
+                //    ViewMovie();
+                //else if (choice == 'D')
+                //    DeleteMovie();
+                //else
+                //    Console.WriteLine("Unkown option");
+
+                switch (choice)
+                {
+                    case 'Q': done = HandleQuit(); break;
+                    case 'A': AddMovie(); break;
+                    case 'V': ViewMovie(); break;
+                    case 'D': DeleteMovie(); break;
+                    default: DisplayError("Unknown option"); break;
+
+                }
+
+            } while (!done);
+
+            //TODO: handle additional stuff
+
+
+
+        }
+
+        private static bool HandleQuit ()
+        {
+            //Display a confermation
+            if (ReadBoolean("Are you sure you want to quit (Y/N)? "))
+                return true;
+
+            return false;
+        }
+
+        static string title;
+        static string description;
+        static int runLength;
+        static int releaseYear;
+        static double reviewRating;
+        static string rating;
+        static bool isClassic;
+
+        static void AddMovie ()
+        {
+            title = ReadString("Enter the Movie title: ", true);
+            description = ReadString("Enter the optional description: ", false);
+
+            runLength = ReadInt32("Enter run length (in minutes): ", 0);
+            releaseYear = ReadInt32("Enter the release year (min 1900): ", 1900);
+
+            //reviewRating;
+            rating = ReadString("Enter the MPAA rating: ", false);
+            isClassic = ReadBoolean("Is this a classic (Y/N)? "); // Optional
+
+        }
+
+        static void ViewMovie ()
+        {
+            if(String.IsNullOrEmpty(title))
+            {
+                Console.WriteLine("No movie available");
+                return;
+            }
+
+            Console.WriteLine($"title) ({ releaseYear}");
+            Console.WriteLine($"Runtime: {runLength} mins");
+            Console.WriteLine($"MPAA Rating: {rating}");
+            Console.WriteLine($"Classic: {isClassic}");
+            Console.WriteLine(description);
+        }
+
+        private static void DeleteMovie ()
+        {
+            if (!ReadBoolean("Are you sure (Y/N)? "))
+                return;
+
+            title = null;
+        }
+
+        static int ReadInt32 ( string message, int minimumValue )
+        {
+            Console.Write(message);
+
+            do
+            {
+                //string input = Console.ReadLine();
+
+                var input = Console.ReadLine();
+
+                //int result = Int32.TryParse(input, result);
+                //int result;
+
+                // if (Int32.TryParse(input, out result))
+                //if(result >= minimumValue)
+
+                if (Int32.TryParse(input, out var result) && result >= minimumValue)
+                    return result;
+
+                DisplayError("The value must be an intergral value >= " + minimumValue);
+
+            } while (true);
+
+            //return -1;
+        }
+
+        static string ReadString ( string message, bool required )
+        {
+            Console.Write(message);
+            do
+            {
+                string input = Console.ReadLine().Trim();
+
+                if (!String.IsNullOrEmpty(input) || !required)
+                    return input;
+
+                DisplayError("Value is required");
+
+            } while (true);
+
+        }
+
+        static void DisplayError ( string message )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static bool ReadBoolean ( string message )
+        {
+            Console.Write(message);
+
+            do
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Y)
+                    return true;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
+
+            } while (true);
+
+            //TODO: Input Validation
+            // not needed anymore return false;
+
+        }
+
+        static char GetInput ()
+        {
+            Console.WriteLine("Movie Library");
+            Console.WriteLine("---------------");
+            Console.WriteLine("".PadLeft(15, '-'));
+
+
+            Console.WriteLine("Add Movie (A)");
+            Console.WriteLine("View (V)");
+            Console.WriteLine("Delete (D)");
+            Console.WriteLine("Quit (Q)");
+
+            //TODO: Input validation
+            //GetInput
+
+            while (true)
+            {
+                string input = Console.ReadLine().Trim();
+
+                
+
+               // if (input == "Q")
+                  //  return 'Q';
+               // else if (input == "A")
+                //    return 'A';
+                //else if (input == "V")
+                 //   return 'V';
+                //else if (input == "D")
+                 //   return 'D';
+                switch (input.ToUpper())
+                {
+                    case "q":
+                    case "Q": return 'Q';
+
+                    case "a":
+                    case "A": return 'A';
+
+                    case "v":
+                    case "V": return 'V';
+
+                    case "d":
+                    case "D": return 'D';
+
+                    //default;
+                }
+
+                DisplayError("Invalid input");
+            };
+
+            //return default(char); //default
+        }
+
+    }
+}
