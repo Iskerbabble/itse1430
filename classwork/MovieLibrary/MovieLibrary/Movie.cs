0568c9c295d1afc104c24a65e56f2c32cf8a4a9a
Benjamin West <benjamin.west496@my.tccd.edu>
2021-09-20 21:31:40 -0500

    it is done

---
+++ classwork/MovieLibrary/MovieLibrary/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MovieLibrary
+{
+    public class Movie
+    {
+        public string title;
+        public string description;
+        public int runLength;
+        public int releaseYear = 1900;
+        public double reviewRating;
+        public string rating;
+        public bool isClassic;
+
+
+    }
+}
